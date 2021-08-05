ccc51dcd09f45de68b6157bcb64a6049a927960a
Quoc Trinh <quoc.trinh@outlook.com>
2021-08-05 10:46:17 -0500

    Fix url base issue

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 angular.module("myMemeApp", ['ngRoute']).config(config);
 
-function config($routeProvider) {
+function config($routeProvider, $locationProvider) {
+    $locationProvider.html5Mode(true);
+
     $routeProvider.when("/meme", {
         templateUrl: "meme/meme.html",
         controller: "MemeController",
         controllerAs: "memeCtrl"
     }).
     when("/", {
-        templateUrl: "/home/home.html",
+        templateUrl: "/meme/home/home.html",
         controller: "HomeController",
         controllerAs: "homeCtrl"
     }).otherwise({
